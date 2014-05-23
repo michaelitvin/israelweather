8552d91c3303f5c69a87ae7ec582ad2661c4a923
Michael Litvin <michael.litvin@gmail.com>
2014-05-23 06:48:33 +0300

    Removed tide forecast fixed Russian translation

--- src/com/litvin/israelweather/MainActivity.java
+++ src/com/litvin/israelweather/MainActivity.java
@@ -42,10 +42,10 @@ public class MainActivity extends ActionBarActivity implements
 	private static final Locale[] LOCALES = {new Locale("en"), new Locale("he"), new Locale("ru")};
 
 	private static final String STATE_SELECTED_NAVIGATION_ITEM = "selected_navigation_item";
-	private static final String[] FRAGMENT_TAGS = {"imsForecastCountryFragment", "imsForecastCitiesFragment", "rainForecastFragmant", "rainRadarFragmant", "tempMapFragment", "tideTableFragment"};
+	private static final String[] FRAGMENT_TAGS = {"imsForecastCountryFragment", "imsForecastCitiesFragment", "rainForecastFragmant", "rainRadarFragmant", "tempMapFragment"/*, "tideTableFragment"*/};
 	
 	private static final int RAIN_RADAR_FRAMES = 12;
-	private Fragment imsForecastCountryFragment, imsForecastCitiesFragment, rainForecastFragmant, rainRadarFragmant, tempMapFragment, tideTableFragment;
+	private Fragment imsForecastCountryFragment, imsForecastCitiesFragment, rainForecastFragmant, rainRadarFragmant, tempMapFragment/*, tideTableFragment*/;
 	private Fragment fragmentArray[];
 	
 	private Bitmap errorBitmap;
@@ -78,7 +78,7 @@ public class MainActivity extends ActionBarActivity implements
 								announceNew(getString(R.string.title_section_rain_forecast)),
 								getString(R.string.title_section_rain_radar),
 								getString(R.string.title_section_temperatures_map),
-								getString(R.string.title_section_tide_table), }), this);
+								/*getString(R.string.title_section_tide_table),*/ }), this);
 		
 		Drawable drw = getResources().getDrawable(R.drawable.bullet_error);
 	    errorBitmap = ((BitmapDrawable)drw).getBitmap();
@@ -130,12 +130,14 @@ public class MainActivity extends ActionBarActivity implements
 		if (tempMapFragment == null)
 			tempMapFragment = ImageFragment.newInstance(fragmentTag, new String[] {getString(R.string.ims_temperatures_map_url)}, savedInstanceState, errorBitmap);
 		
+		/*
 		fragmentTag = FRAGMENT_TAGS[5];
 		tideTableFragment = (ImageFragment)getSupportFragmentManager().findFragmentByTag(fragmentTag);
 		if (tideTableFragment == null)
 			tideTableFragment = ImageFragment.newInstance(fragmentTag, new String[] {getString(R.string.isramar_tide_table_url)}, savedInstanceState, errorBitmap);
+		*/
 		
-		fragmentArray = new Fragment[] {imsForecastCountryFragment, imsForecastCitiesFragment, rainForecastFragmant, rainRadarFragmant, tempMapFragment, tideTableFragment};
+		fragmentArray = new Fragment[] {imsForecastCountryFragment, imsForecastCitiesFragment, rainForecastFragmant, rainRadarFragmant, tempMapFragment/*, tideTableFragment*/};
 	}
 
 	
